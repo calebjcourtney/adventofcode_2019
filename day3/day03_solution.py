04a831e6f6d8d26497a72764bc46d8abcf7d7c7b
calebjcourtney <brokenfoot@rocketmail.com>
2019-12-06 10:12:17 -0800

    update day 3

--- day3/day03_solution.py
+++ day3/day03_solution.py
@@ -1,4 +1,4 @@
-def get_points_steps(in_text, point = None):
+def get_points_steps(in_text: str, point = None) -> tuple:
     points = []
     steps = 0
     cursor = [0, 0]
@@ -49,36 +49,36 @@ def get_points_steps(in_text, point = None):
     return set(points), steps
 
 
-def calculate_intersection(in_text, point = None):
+def get_intersect_steps(in_text: str, point = None) -> tuple:
     first_points, first_steps = get_points_steps(in_text[0], point)
     second_points, second_steps = get_points_steps(in_text[1], point)
 
     return (list(first_points & second_points), first_steps + second_steps)
 
 
-def get_closest_intersection(in_text):
-    intersections, steps = calculate_intersection(in_text)
+def get_part_one(in_text: str) -> int:
+    intersections, steps = get_intersect_steps(in_text)
     return min([abs(x[0]) + abs(x[1]) for x in intersections])
 
 
-def get_min_steps(in_text):
-    intersections, steps = calculate_intersection(in_text)
-    return min([calculate_intersection(in_text, intersect)[1] for intersect in intersections])
+def get_part_two(in_text: str) -> int:
+    intersections, steps = get_intersect_steps(in_text)
+    return min([get_intersect_steps(in_text, intersect)[1] for intersect in intersections])
 
 
 if __name__ == '__main__':
-    # check to make sure the function passes tests given in the setup
-    assert(get_closest_intersection(['R8,U5,L5,D3', 'U7,R6,D4,L4']) == 6)
-    assert(get_closest_intersection(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]) == 159)
-    assert(get_closest_intersection(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]) == 135)
-
-    assert(get_min_steps(['R8,U5,L5,D3', 'U7,R6,D4,L4']) == 30)
-    assert(get_min_steps(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]) == 610)
-    assert(get_min_steps(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]) == 410)
-
     # read the data
     in_file = open('input.txt', 'r')
     data = [x for x in in_file.read().split('\n')]
 
-    print(get_closest_intersection(data))
-    print(get_min_steps(data))
+    # part one
+    assert(get_part_one(['R8,U5,L5,D3', 'U7,R6,D4,L4']) == 6)
+    assert(get_part_one(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]) == 159)
+    assert(get_part_one(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]) == 135)
+    print(get_part_one(data))
+
+    # part two
+    assert(get_part_two(['R8,U5,L5,D3', 'U7,R6,D4,L4']) == 30)
+    assert(get_part_two(["R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83"]) == 610)
+    assert(get_part_two(["R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"]) == 410)
+    print(get_part_two(data))
