f1e654da15d9b90bfd3024e5dceb24faad1b3e26
calebjcourtney <brokenfoot@rocketmail.com>
2019-12-06 17:17:49 -0800

    pull if statements out to dict mapping the int to functions

--- day5/solve.py
+++ day5/solve.py
@@ -23,15 +23,82 @@ has_output = {
 }
 
 
-def process_index(index_point):
-    instruction = DATA[index_point]
-    op_code = instruction % 100
-
+def parse_modes(instruction):
     # get the modes being used
     mode = []
     mode.append(instruction % 1000 // 100)
     mode.append(instruction % 10000 // 1000)
     mode.append(instruction % 100000 // 10000)
+
+    return mode
+
+
+def op_add(loc, op_inputs, index_point):
+    DATA[loc] = op_inputs[0] + op_inputs[1]
+    return index_point
+
+
+def op_mutliplication(loc, op_inputs, index_point):
+    DATA[loc] = op_inputs[0] * op_inputs[1]
+    return index_point
+
+
+def op_input(loc, op_inputs, index_point):
+    DATA[loc] = input_instruction
+    return index_point
+
+
+def op_output(loc, op_inputs, index_point):
+    outputs.append(op_inputs[0])
+    return index_point
+
+
+def op_jumpiftrue(loc, op_inputs, index_point):
+    if op_inputs[0] != 0:
+        index_point = op_inputs[1]
+
+    return index_point
+
+
+def op_jumpiffalse(loc, op_inputs, index_point):
+    if op_inputs[0] == 0:
+        index_point = op_inputs[1]
+
+    return index_point
+
+
+def op_lessthan(loc, op_inputs, index_point):
+    DATA[loc] = 1 if op_inputs[0] < op_inputs[1] else 0
+    return index_point
+
+
+def op_equals(loc, op_inputs, index_point):
+    DATA[loc] = 1 if op_inputs[0] == op_inputs[1] else 0
+    return index_point
+
+
+def op_kill(loc, op_inputs, index_point):
+    return None
+
+
+op_function = {
+    1: op_add,
+    2: op_mutliplication,
+    3: op_input,
+    4: op_output,
+    5: op_jumpiftrue,
+    6: op_jumpiffalse,
+    7: op_lessthan,
+    8: op_equals,
+    99: op_kill
+}
+
+
+def process_index(index_point):
+    instruction = DATA[index_point]
+    op_code = instruction % 100
+
+    mode = parse_modes(instruction)
 
     index_point += 1
 
@@ -41,47 +108,14 @@ def process_index(index_point):
         op_inputs.append(DATA[index_point] if mode[i] == 1 else DATA[DATA[index_point]])
         index_point += 1
 
+    loc = None
+
     # has an output location
     if has_output[op_code]:
         loc = DATA[index_point] if mode[-1] == 0 else None
         index_point += 1
 
-    if op_code == 1:
-        # addition
-        DATA[loc] = op_inputs[0] + op_inputs[1]
-
-    elif op_code == 2:
-        # mutliplication
-        DATA[loc] = op_inputs[0] * op_inputs[1]
-
-    elif op_code == 3:
-        # input
-        DATA[loc] = input_instruction
-
-    elif op_code == 4:
-        # append to outputs
-        outputs.append(op_inputs[0])
-
-    elif op_code == 5:
-        # jump-if-true
-        if op_inputs[0] != 0:
-            index_point = op_inputs[1]
-
-    elif op_code == 6:
-        # jump-if-false
-        if op_inputs[0] == 0:
-            index_point = op_inputs[1]
-
-    elif op_code == 7:
-        # less than
-        DATA[loc] = 1 if op_inputs[0] < op_inputs[1] else 0
-
-    elif op_code == 8:
-        # equals
-        DATA[loc] = 1 if op_inputs[0] == op_inputs[1] else 0
-
-    elif op_code == 99:
-        return None
+    index_point = op_function[op_code](loc, op_inputs, index_point)
 
     return index_point
 
