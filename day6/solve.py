0b149b5c024ae7c20d661d55aefc69e225a59a1e
calebjcourtney <brokenfoot@rocketmail.com>
2019-12-06 16:25:15 -0800

    networkx is the right implementation for this - no nasty pandas code

--- day6/solve.py
+++ day6/solve.py
@@ -1,55 +1,34 @@
-import pandas as pd
+import networkx as nx
 
 
-def process_data(num_list, input_val = 1):
-    total_df = pd.DataFrame(
-        {
-            'source': [item.split(')')[0] for item in num_list],
-            'target': [item.split(')')[1] for item in num_list]
-        }
+def part_one(input_data):
+    graph_part_one = nx.DiGraph()
+    graph_part_one.add_edges_from(input_data)
 
-    )
+    orbit_count = sum([get_pred_sum(node, graph_part_one) for node in graph_part_one.nodes])
 
-    origin_df = total_df.loc[total_df['source'] == 'COM']
-    origin_df.rename(columns = {'source': 'level_1', 'target': 'level_2'}, inplace = True)
+    return orbit_count
 
-    current_df = pd.DataFrame()
 
-    x = 2
-    while origin_df['level_{}'.format(x - 1)].tolist() != origin_df['level_{}'.format(x)].tolist():
-        origin_df = origin_df.merge(total_df, left_on = 'level_{}'.format(x), right_on = 'source', how = 'left')
-        origin_df.rename(columns = {'target': 'level_{}'.format(x + 1)}, inplace = True)
-        origin_df.drop('source', axis = 1, inplace = True)
+def get_pred_sum(node, graph_part_one):
+    predecessors = list(graph_part_one.predecessors(node))
+    predecessors_sum = len(predecessors)
 
-        if not origin_df.empty:
-            current_df = origin_df.copy()
+    # recursion actually worked on this problem!
+    return sum([predecessors_sum + get_pred_sum(pred, graph_part_one) for pred in predecessors])
 
-        x += 1
 
-    current_df.drop_duplicates(inplace = True)
+def part_two(input_data):
+    graph_part_two = nx.Graph()
+    graph_part_two.add_edges_from(input_data)
 
-    connections = 0
-    for x in range(3, 294):
-        temp_df = current_df[[f'level_{a}' for a in range(1, x)]]
-        temp_df.dropna(inplace = True)
-        temp_df.drop_duplicates(inplace = True)
+    path = nx.shortest_path(graph_part_two, 'YOU', 'SAN')
 
-        temp_df['count'] = temp_df.count(axis = 1)
-        temp_df['count'] -= 1
-
-        connections += temp_df['count'].sum()
-
-    print("direct and indirect orbits:", connections)
-
-    current_df = current_df[(current_df.values == "SAN") | (current_df.values == "YOU")]
-
-    transfers = len(list(set(current_df.values[0]).difference(current_df.values[1]))) + len(list(set(current_df.values[1]).difference(current_df.values[0]))) - 2
-
-    print("transfers to get to Santa:", transfers)
+    return len(path[2:-1])
 
 
 if __name__ == '__main__':
-    # part one
-    DATA = open('input.txt', 'r').read().split('\n')
+    DATA = [[record.split(')')[0], record.split(')')[1]] for record in open('input.txt', 'r').read().split('\n')]
 
-    process_data(DATA)
+    print(f"part one solution: {part_one(DATA)}")
+    print(f"part two solution: {part_two(DATA)}")
